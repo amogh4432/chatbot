518abc58f29f0df55d21b318b956bc0cd11a3d4f
Pranav Supsande <pranavsupsande1952@gmail.com>
2023-04-28 00:55:48 +0530

    Data Changed

--- src/data.js
+++ src/data.js
@@ -1,21 +1,27 @@
 export const data =[
     {
-        name:'camera1',
-        imageSrc:'https://cpimg.tistatic.com/05962686/b/4/HD-Color-Outdoor-CCTV-Camera.jpg',
+        name:'Bullet Camera',
+        imageSrc:'https://5.imimg.com/data5/OY/PF/MY-10659089/utdoor-cctv-camera-500x500.jpg',
         price:'500',
-        discription:'mgjadjflijskflhnsjlkajfkngkjdbfnknsdkfjnvdsldkbvkjdf.ngvndkfvnkjfvnklnvkjfznv.',
+        description:'Bullet cameras are long and cylindrical in shape and are ideal for outdoor use.',
         warranty:'1 year',
     },{
-        name:'camera1',
-        imageSrc:'https://cpimg.tistatic.com/05962686/b/4/HD-Color-Outdoor-CCTV-Camera.jpg',
+        name:'Dome Camera',
+        imageSrc:'https://4.imimg.com/data4/BA/RT/MY-15951097/dome-500x500.jpg',
         price:'500',
-        discription:'mgjadjflijskflhnsjlkajfkngkjdbfnknsdkfjnvdsldkbvkjdf.ngvndkfvnkjfvnklnvkjfznv.',
+        description:'The dome camera is one most commonly used for indoor and outdoor security and surveillance.',
         warranty:'1 year',
     },{
-        name:'camera1',
-        imageSrc:'https://cpimg.tistatic.com/05962686/b/4/HD-Color-Outdoor-CCTV-Camera.jpg',
+        name:'Network Cameras',
+        imageSrc:'https://4.imimg.com/data4/WV/KF/MY-3778805/network-ip-camera-500x500.jpg',
         price:'500',
-        discription:'mgjadjflijskflhnsjlkajfkngkjdbfnknsdkfjnvdsldkbvkjdf.ngvndkfvnkjfvnklnvkjfznv.',
+        description:'These cameras share the images across the internet, so CCTV footage can be easily accessed.',
+        warranty:'1 year',
+    },{
+        name:'Day/Night Camera',
+        imageSrc:'https://5.imimg.com/data5/LI/FI/MY-4121490/ir-day-night-cctv-camera-500x500.jpg',
+        price:'500',
+        description:'Capable of operating in both normal and poorly lit environments.',
         warranty:'1 year',
     }
 ]
